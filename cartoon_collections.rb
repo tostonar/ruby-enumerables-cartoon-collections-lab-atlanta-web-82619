d329a2cc9241333243435ef3576b6e544790621f
Andrea Toston <tostonar@gmail.com>
2019-07-26 13:59:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(arr)
   arr.map { |e| "#{e.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |e| e.length > 4  }
 end
 
 def find_the_cheese# code an argument here
