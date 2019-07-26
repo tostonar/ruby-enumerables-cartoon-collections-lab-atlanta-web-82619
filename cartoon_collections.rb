0987894cbf951f541911d28dfe8caaab6eec241f
Andrea Toston <tostonar@gmail.com>
2019-07-26 13:57:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(arr)
-  # Your code here
+  arr.map { |e| "#{e.capitalize}!" }
 end
 
 def long_planeteer_calls# code an argument here
